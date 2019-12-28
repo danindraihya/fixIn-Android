c3367da5a832164f4a2344479221aab525762704
SeptianWahyuni <septianwahyuni76@gmail.com>
2019-12-27 18:40:34 -1200

    penambahan layout

---
+++ app/src/main/java/com/example/fixinnew/aturSandi.java
@@ -0,0 +1,14 @@
+package com.example.fixinnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aturSandi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atur_sandi);
+    }
+}
---
+++ app/src/main/java/com/example/fixinnew/berhasilAtur.java
@@ -0,0 +1,14 @@
+package com.example.fixinnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class berhasilAtur extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berhasil_atur);
+    }
+}
