cdfb183072ec7f807f9bfe48b6c1851ce6fa051d
SeptianWahyuni <septianwahyuni76@gmail.com>
2020-01-02 02:20:50 +0700

    ubah_profil

---
+++ app/src/main/java/com/example/fixinnew/ubah_profil.java
@@ -0,0 +1,14 @@
+package com.example.fixinnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ubah_profil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_profil);
+    }
+}
